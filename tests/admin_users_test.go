bf4e76fc907c65064d32fedd7f6017942b37233f
Alex Browne <stephenalexbrowne@gmail.com>
2014-12-26 15:09:20 -0500

    Test create admin users

--- tests/admin_users_test.go
+++ tests/admin_users_test.go
@@ -1,1 +1,30 @@
 package tests
+
+import (
+	"github.com/albrow/fipple"
+	"testing"
+)
+
+func TestAdminUsersCreate(t *testing.T) {
+	rec := fipple.NewRecorder(t, testUrl)
+
+	// Get a valid token
+	token, err := getAdminTestToken()
+	if err != nil {
+		panic(err)
+	}
+
+	// Created an authenticated request
+	req := rec.NewRequestWithData("POST", "/admin/users", map[string]string{
+		"email":           "test@example.com",
+		"password":        "password",
+		"confirmPassword": "password",
+	})
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	// Send the request and check the response
+	res := rec.Do(req)
+	res.AssertOk()
+	res.AssertBodyContains("admin")
+	res.AssertBodyContains(`"email": "test@example.com"`)
+}
